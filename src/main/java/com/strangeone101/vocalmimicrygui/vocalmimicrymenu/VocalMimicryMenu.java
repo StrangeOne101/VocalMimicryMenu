0cf9d960af075f0f2e023df0c5dd348e17e5b33e
StrangeOne101 <strange.toby@gmail.com>
2022-07-18 11:40:31 +1200

    Fixed permission issue

--- src/main/java/com/strangeone101/vocalmimicrygui/vocalmimicrymenu/VocalMimicryMenu.java
+++ src/main/java/com/strangeone101/vocalmimicrygui/vocalmimicrymenu/VocalMimicryMenu.java
@@ -5,6 +5,7 @@ import me.simplicitee.project.addons.ability.air.VocalMimicry;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VocalMimicryMenu extends JavaPlugin {
@@ -20,6 +21,11 @@ public final class VocalMimicryMenu extends JavaPlugin {
 
         Bukkit.getPluginManager().registerEvents(new Listener(), this);
 
+        Permission perm = Bukkit.getPluginManager().getPermission("bending.command.vocalmenu");
+        Permission parent = Bukkit.getPluginManager().getPermission("bending.player");
+        perm.addParent(parent, true);
+
+        getLogger().info("VocalMimicryMenu has been enabled!");
     }
 
     @Override
